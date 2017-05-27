6ed8bdcc74423b4b3db350f089f2810e562a19ce
NgocTu <dhhtuss512@gmail.com>
2017-05-27 20:40:59 +0700

    Lan dau tien di hoc

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+/**
+ * Created by Admin on 05/27/17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("lau dau tien di hoc");
+
+    }
+
+
+}
